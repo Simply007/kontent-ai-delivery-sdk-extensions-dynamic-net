8acf4ea1f985dd10c62a02f12b7598ff5d28bc4b
Ondřej Chrastina <ondrejch@kontent.ai>
2023-02-03 12:30:19 +0100

    rewrite test to cover all value element types

--- Simply007.Kontent.Ai.Delivery.Extensions.Dynamic.Tests/Extenstions/IDeliveryItemResponseExtensionsTests.cs
+++ Simply007.Kontent.Ai.Delivery.Extensions.Dynamic.Tests/Extenstions/IDeliveryItemResponseExtensionsTests.cs
@@ -2,15 +2,19 @@ using RichardSzalay.MockHttp;
 using Kontent.Ai.Delivery;
 using Kontent.Ai.Delivery.Builders.DeliveryClient;
 using FluentAssertions;
+using Kontent.Ai.Delivery.Abstractions;
 
 namespace Simply007.Kontent.Ai.Delivery.Extensions.Dynamic
 {
     public class IDeliveryItemResponseExtensionsTests
     {
+        private const string itemCodename = "content_item";
+        private const string itemJsonCodename = itemCodename + ".json";
         private readonly string _baseUrl;
         private readonly MockHttpMessageHandler _mockHttp;
         private readonly Guid _guid;
         private readonly string _projectId;
+        private readonly IDeliveryClient _client;
 
         private HttpClient httpClient => _mockHttp.ToHttpClient();
 
@@ -21,25 +25,33 @@ namespace Simply007.Kontent.Ai.Delivery.Extensions.Dynamic
             _projectId = _guid.ToString();
             _baseUrl = $"https://deliver.kontent.ai/{_projectId}";
             _mockHttp = new MockHttpMessageHandler();
+            string url = $"{_baseUrl}/items/";
+
+            _mockHttp
+                .When($"{url}{itemCodename}")
+                .Respond("application/json", File.ReadAllText(Path.Combine(Environment.CurrentDirectory, $"Data{Path.DirectorySeparatorChar}{itemJsonCodename}")));
+
+            _client = DeliveryClientBuilder.WithProjectId(_projectId)
+                .WithDeliveryHttpClient(new DeliveryHttpClient(httpClient))
+                .Build();
         }
 
         [Fact]
         public async Task GetElementValue_Text_ReturnsCorrectValue()
         {
-            string url = $"{_baseUrl}/items/";
-
-            _mockHttp
-                .When($"{url}text_element_item")
-                .Respond("application/json", await File.ReadAllTextAsync(Path.Combine(Environment.CurrentDirectory, $"Data{Path.DirectorySeparatorChar}text_element_item.json")));
-
-            var client = DeliveryClientBuilder.WithProjectId(_projectId)
-                .WithDeliveryHttpClient(new DeliveryHttpClient(httpClient))
-                .Build();
-
-            var response = await client.GetItemAsync<object>("text_element_item");
+            var response = await _client.GetItemAsync<object>(itemCodename);
 
             var element = response.GetElementValue<string>("text_element");
-            element.Should().Be("Donec elit libero sodales nec.");
+            element.Should().Be("Ut tincidunt tincidunt erat");
+        }
+
+        [Fact]
+        public async Task GetElementValue_Number_ReturnsCorrectValue()
+        {
+            var response = await _client.GetItemAsync<object>(itemCodename);
+
+            var element = response.GetElementValue<decimal>("number_element");
+            element.Should().Be(42);
         }
     }
 }
